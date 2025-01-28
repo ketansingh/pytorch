f94850c385e9b6117147a79999dda1f424632f87
Animesh Jain <anijain@umich.edu>
2025-01-28 15:28:55 -0800

    [dynamo][builtin-skipfiles-cleanup] Remove random
    ghstack-source-id: d73e87a7e531689f8c740c31cbd06760da59574d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/145804

--- torch/_dynamo/trace_rules.py
+++ torch/_dynamo/trace_rules.py
@@ -8,7 +8,6 @@ import importlib
 import inspect
 import operator
 import os
-import random
 import re
 import sys
 import traceback
@@ -3144,7 +3143,6 @@ def is_numpy_type_info(obj) -> bool:
 BUILTIN_SKIPLIST = (
     collections,
     inspect,
-    random,
     types,
     traceback,
     unittest,
